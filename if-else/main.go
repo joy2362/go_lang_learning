5fec8ffcc3e58986142ed4bc3137949c6a73a29b
joy2362 <abdullahzahidjoy@gmail.com>
2023-09-14 02:28:40 +0600

    condition

--- if-else/main.go
+++ if-else/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main()  {
 	welcome := "welcome"
@@ -15,4 +18,30 @@ func main()  {
 	} else {
 		fmt.Println("Login successfully!!")
 	}
+
+	//switch case
+	diceNumber := rand.Intn(6) + 1
+	fmt.Println(diceNumber)
+
+	switch diceNumber {
+	case 1:
+		fmt.Println("you can open")
+	case 2 : 
+		fmt.Println("you can move 2 spot")
+	case 3 : 
+		fmt.Println("you can move 3 spot")
+		fallthrough
+	case 4 : 
+		fmt.Println("you can move 4 spot")
+		fallthrough // it print next case with out condition check
+	case 5 : 
+		fmt.Println("you can move 5 spot")
+	case 6 : 
+		fmt.Println("you can move 6 spot")
+	default: 
+		fmt.Println("What was that")
+
+	}
+
+
 }

81b7dd6b05aaee00bed91c06928a8568a3a05977
joy2362 <abdullahzahidjoy@gmail.com>
2023-09-18 00:26:26 +0600

    webRequest

---
+++ webRequest/go.mod
@@ -0,0 +1,3 @@
+module MyWebRequest
+
+go 1.21.1
---
+++ webRequest/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const URL = "https://lco.dev"
+
+func main() {
+	welcome := "welcome"
+	fmt.Println(welcome)
+ 
+	res , err :=http.Get(URL)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response type is ", res.Status)
+	fmt.Printf("%T \n" , res)
+
+	defer res.Body.Close()
+
+	dataByte , err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(dataByte)
+
+	fmt.Println(content)
+
+}

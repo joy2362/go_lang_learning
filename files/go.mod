897a453c76d5d3b76858650c92495f4eaeffce9e
joy2362 <abdullahzahidjoy@gmail.com>
2023-09-18 00:04:32 +0600

    file

---
+++ files/go.mod
@@ -0,0 +1,3 @@
+module MyFile
+
+go 1.21.1
---
+++ files/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	welcome := "welcome"
+	fmt.Println(welcome)
+
+	content := "hello world"
+
+	file , err :=os.Create("./some.txt")
+
+	checkNillError(err)
+
+	length , err := io.WriteString(file, content)
+
+	checkNillError(err)
+
+
+	fmt.Println(length)
+
+	defer file.Close()
+
+	readFile("./some.txt")
+
+}
+
+//read file 
+
+func readFile(fileName string){
+	dataByte , err := os.ReadFile(fileName)
+
+	checkNillError(err)
+
+	fmt.Println(dataByte)
+	fmt.Println(string(dataByte))
+}
+
+func checkNillError(err error){
+	if err != nil{
+		panic(err)
+	}
+}
